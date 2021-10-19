04b73da61d78fea381178bf0f3ff798fc641e4c4
kamrulhaider <kamrulhaider.ssb@gmail.com>
2021-10-19 09:56:27 +0600

    email-auth-add

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, updateProfile } from "firebase/auth";
 import { useEffect, useState } from "react";
 import initializeAuthentication from "../pages/Login/Firebase/firebase.init";
 
@@ -7,6 +7,11 @@ initializeAuthentication()
 const useFirebase = () => {
     const [user, setUser] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [isLogin, setIsLogin] = useState(false);
 
     const auth = getAuth();
 
@@ -20,6 +25,84 @@ const useFirebase = () => {
                 setUser(result.user)
             })
             .finally(() => setIsLoading(false));
+    }
+    const toggleLogin = e => {
+        setIsLogin(e.target.checked)
+    }
+
+    const handleNameChange = e => {
+        setName(e.target.value);
+    }
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value)
+    }
+
+    const handleRegistration = e => {
+        e.preventDefault();
+        console.log(email, password);
+        if (password.length < 6) {
+            setError('Password Must be at least 6 characters long.')
+            return;
+        }
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setError('Password Must contain 2 upper case');
+            return;
+        }
+
+        if (isLogin) {
+            processLogin(email, password);
+        }
+        else {
+            registerNewUser(email, password);
+        }
+
+    }
+
+    const processLogin = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setError('');
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
+
+    const registerNewUser = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setError('');
+                verifyEmail();
+                setUserName();
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
+
+    const setUserName = () => {
+        updateProfile(auth.currentUser, { displayName: name })
+            .then(result => { })
+    }
+
+    const verifyEmail = () => {
+        sendEmailVerification(auth.currentUser)
+            .then(result => {
+                console.log(result);
+            })
+    }
+
+    const handleResetPassword = () => {
+        sendPasswordResetEmail(auth, email)
+            .then(result => { })
     }
 
     // statechage observer
@@ -47,7 +130,15 @@ const useFirebase = () => {
     return {
         user,
         isLoading,
+        isLogin,
+        error,
         signInUsingGoogle,
+        toggleLogin,
+        handleEmailChange,
+        handleNameChange,
+        handlePasswordChange,
+        handleRegistration,
+        handleResetPassword,
         logOut
     }
 }
--- src/pages/Login/Firebase/Login/Login.js
+++ src/pages/Login/Firebase/Login/Login.js
@@ -1,12 +1,39 @@
-import React from 'react';
 import useAuth from '../../../../hooks/useAuth';
+import './Login.css'
 
 const Login = () => {
 
-    const { signInUsingGoogle } = useAuth();
+    const { signInUsingGoogle, handleRegistration, isLogin, handleNameChange, handleEmailChange, handlePasswordChange, toggleLogin, error, handleResetPassword } = useAuth();
+
     return (
-        <div>
-            <button onClick={signInUsingGoogle}>Google Login</button>
+        <div className="login">
+            <div className="login-form">
+                <form onSubmit={handleRegistration}>
+                    <h3 className="text-center mb-3 fw-bold">Please {isLogin ? 'Login' : 'Register'}</h3>
+                    {!isLogin && <div className="mb-3">
+                        <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
+                        <input type="text" onBlur={handleNameChange} className="form-control" id="inputName" placeholder="Your Name" />
+                    </div>}
+                    <div className="mb-3">
+                        <label className="form-label">Email address</label>
+                        <input onBlur={handleEmailChange} type="email" className="form-control" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Password</label>
+                        <input onBlur={handlePasswordChange} type="password" className="form-control" />
+                    </div>
+                    <div className="form-check">
+                        <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1" />
+                        <label className="form-check-label" htmlFor="gridCheck1">
+                            Already Registered?
+                        </label>
+                    </div>
+                    <div className="row mb-3 text-danger">{error}</div>
+                    <button type="submit" className="btn btn-primary"> {isLogin ? 'Login' : 'Register'}</button>
+                    <button type="button" onClick={handleResetPassword} className="btn btn-secondary ms-3">Reset Password</button>
+                </form>
+                <button onClick={signInUsingGoogle} className="btn btn-warning mt-3 text-center">SignIn Using Google</button>
+            </div>
         </div>
     );
 };
